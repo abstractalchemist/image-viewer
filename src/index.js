3b71ea1e1d66461b4a96a25ddc55f383ba4eef64
Jason Hirata <jhirata@integrity-apps.com>
2016-04-15 18:08:33 -1000

    trying scrollfire

--- src/index.js
+++ src/index.js
@@ -31,12 +31,16 @@ const ImageCol = React.createClass({
 
 const Main = React.createClass({
     getInitialState() {
-	return { images: []}
+	return { images: [], start:0, end:10}
     },
     componentWillMount() {
-	Rx.Observable.fromPromise(new Promise( (resolve, reject) => {
+	this.onBottom();
+	
+    },
+    getImages() {
+    	Rx.Observable.fromPromise(new Promise( (resolve, reject) => {
 	    let ajax = new XMLHttpRequest();
-	    ajax.open("GET", "http://localhost:3000/images?start=0&end=10", true);
+	    ajax.open("GET", "http://localhost:3000/images?start=" + this.state.start + "&end=" + this.state.end, true);
 	    ajax.onreadystatechange = _ => {
 		if(ajax.readyState == 4) {
 		    if(ajax.status == 200) {
@@ -57,6 +61,13 @@ const Main = React.createClass({
 	    .subscribe(data => {
 		this.setState({images: data})
 	    });
+	
+    },
+    onBottom() {
+	console.log("hit bottom");
+	this.setState({start:0, end: this.state.end + 10});
+	Materialize.scrollFire([{selector: '#bottom', offset: 0, callback: this.onBottom}]);
+	getImages();
     },
     render() {
 	return (<div className="container">
@@ -74,11 +85,13 @@ const Main = React.createClass({
 				<ImageCol images={image1} width={3} key="imageCol1"/>
 				<ImageCol images={image2} width={3} key="imageCol2"/>
 				<ImageCol images={image3} width={3} key="imageCol3"/>
+
 				</div>)
 	
 		    }
 		})()
 		}
+		<div id="bottom"></div>
 		</div>
 	       );
     }
